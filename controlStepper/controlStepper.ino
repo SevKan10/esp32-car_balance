22133d60e63ddc04bef5bd4dd40870e94d8e370f
KKhang <127233909+SevKan10@users.noreply.github.com>
2025-08-21 00:20:17 +0700

    Update controlStepper.ino

--- controlStepper/controlStepper.ino
+++ controlStepper/controlStepper.ino
@@ -11,9 +11,16 @@
 #define DIR1  35  // Chân DIR1 dùng để xác định chiều quay của step
 #define STEP2 14  // Chân xung
 #define DIR2  27  // Chân DIR1 dùng để xác định chiều quay của step
+
+int R = 2;
+int D = 400;
+String inputString = "";
+bool stringComplete = false;
+
 void setup() 
 {
   Serial.begin(9600);
+  Serial.println("R,D (Ex: 200,500):");
 
   pinMode(EN, OUTPUT);
   pinMode(STEP1, OUTPUT);
@@ -27,15 +34,45 @@ void setup()
 
 void loop() 
 {
+  // Doc du lieu tu Serial
+  while (Serial.available()) 
+  {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {stringComplete = true;}
+  }
+
+  // Xu ly khi chuoi da hoan chinh
+  if (stringComplete) 
+  {
+    inputString.trim();
+    int commaIndex = inputString.indexOf(',');
+
+    if (commaIndex != -1) 
+    {
+      String sR = inputString.substring(0, commaIndex); // Từ vị trí đầu tới ngay trước index
+      String sD = inputString.substring(commaIndex + 1); // Từ ngay index + 1 đơn vị
+
+      R = sR.toInt();
+      D = sD.toInt();
+    } 
+    else {Serial.println("FAILED R,D");}
+
+    inputString = "";
+    stringComplete = false;
+  }
+
   digitalWrite(DIR1, 0);
   digitalWrite(DIR2, 0);
-  controlStep(2, 400);
+  controlStep(R, D);
 
   delay(1000);
 
   digitalWrite(DIR1, 1);
   digitalWrite(DIR2, 1);
-  controlStep(1, 1000);
+  controlStep(R, D);
 
   delay(1000);
+
 }
+
