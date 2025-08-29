b104e79d35d9f090e82754555f253cb34e126881
KKhang <127233909+SevKan10@users.noreply.github.com>
2025-08-29 21:14:40 +0700

    update pid

--- testPID/PID.ino
+++ testPID/PID.ino
@@ -5,10 +5,8 @@ void updatePID(float angle, double deltaTime)
   derivative = (error - lastError)/deltaTime; // Tính vi phân
   output =  Kp*error + Ki*integral + Kd*derivative; // PID
   lastError = error;
+  return (output);
 
-  // Giới hạn output để tránh quá lớn hoặc quá nhỏ
-  if (output > 500) output = 500;
-  if (output < -500) output = -500;
   Serial.print("PID: "); Serial.println(output);
   // controlStep(output);
   // driveStepper(output);
--- testPID/controlStep.ino
+++ testPID/controlStep.ino
@@ -22,33 +22,23 @@
 //   }
 // }
 
-void driveStepper(float pidOut) {
+void driveStepper(float pidOut) 
+{
   // Giới hạn lại để không quá nhanh
   if (pidOut > 500) pidOut = 500;
   if (pidOut < -500) pidOut = -500;
 
   // Xác định chiều quay
-  if (pidOut > 0) {
+  if (pidOut > 0) 
+  {
     digitalWrite(DIR1, 0);
     digitalWrite(DIR2, 1);
-  } else {
+  } 
+  else if (pidOut < 0)  
+  {
     digitalWrite(DIR1, 1);
     digitalWrite(DIR2, 0);
   }
 
-  // Chuyển đổi PID -> tần số (steps/s)
-  float freq = fabs(pidOut) * 3;  // hằng số 3 là hệ số G, bạn chỉnh thử nghiệm
-  if (freq < 50) return;          // dưới 50 steps/s thì bỏ qua để tránh rung
-
-  // Tính chu kỳ xung
-  unsigned long period = 1000000.0 / freq; // microseconds per step
-
-  // Phát 1 xung STEP
-  digitalWrite(STEP1, HIGH);
-  digitalWrite(STEP2, HIGH);
-  delayMicroseconds(100);  // xung tối thiểu 2us cho A4988
-  digitalWrite(STEP1, LOW);
-  digitalWrite(STEP2, LOW);
-  delayMicroseconds(period - 100);
 }
 
